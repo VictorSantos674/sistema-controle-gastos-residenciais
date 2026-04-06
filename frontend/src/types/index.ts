8b7dec826a2cb30c7dd4f14de94b351278a6ef6e
Victor Souza Santos <victor.souza@equipeblue.com.br>
2026-04-06 19:31:04 -0300

    feat: definir tipos TypeScript compartilhados

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,56 @@
+export type Finalidade = "Despesa" | "Receita" | "Ambas";
+export type TipoTransacao = "Despesa" | "Receita";
+
+export interface Pessoa {
+  id: number;
+  nome: string;
+  idade: number;
+}
+
+export interface Categoria {
+  id: number;
+  descricao: string;
+  finalidade: Finalidade;
+}
+
+export interface Transacao {
+  id: number;
+  descricao: string;
+  valor: number;
+  tipo: TipoTransacao;
+  categoriaId: number;
+  categoriaDescricao: string;
+  pessoaId: number;
+  pessoaNome: string;
+}
+
+export interface TotalPorPessoa {
+  pessoaId: number;
+  nomePessoa: string;
+  totalReceitas: number;
+  totalDespesas: number;
+  saldo: number;
+}
+
+export interface RelatorioPorPessoa {
+  pessoas: TotalPorPessoa[];
+  totalGeralReceitas: number;
+  totalGeralDespesas: number;
+  saldoLiquido: number;
+}
+
+export interface TotalPorCategoria {
+  categoriaId: number;
+  descricaoCategoria: string;
+  finalidade: Finalidade;
+  totalReceitas: number;
+  totalDespesas: number;
+  saldo: number;
+}
+
+export interface RelatorioPorCategoria {
+  categorias: TotalPorCategoria[];
+  totalGeralReceitas: number;
+  totalGeralDespesas: number;
+  saldoLiquido: number;
+}
